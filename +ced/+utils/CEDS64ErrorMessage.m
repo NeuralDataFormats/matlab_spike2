19f8042a8ffe9942601164e240a79467386d9683
Jim Hokanson <jim.hokanson@gmail.com>
2026-05-24 21:16:16 -0500

    Update CEDS64ErrorMessage.m
    cleaning up errors - still need to add throughout code

--- +ced/+utils/CEDS64ErrorMessage.m
+++ +ced/+utils/CEDS64ErrorMessage.m
@@ -1,68 +1,94 @@
-function [errmsg] = CEDS64ErrorMessage(iErrorCode)
+function err_msg = CEDS64ErrorMessage(iErrorCode)
 %
-%   errmsg = ced.utils.CEDS64ErrorMessage(iErrorCode)
+%   err_msg = ced.utils.CEDS64ErrorMessage(iErrorCode)
 %
 %   The CEDS64ErrorMessage in the library throws a warning message
 %   rather than returning a string. This version returns the string.
 %
-%
+%   Improvements
+%   ------------
+%   1) 
+
+if ~(isnumeric(iErrorCode) && isscalar(iErrorCode) && isfinite(iErrorCode))
+    error('iErrorCode must be a finite numeric scalar.')
+end
+
+iErrorCode = double(iErrorCode);
 
 switch iErrorCode
+    case 0
+        err_msg = 'S64_OK: There was no error.';
     case -1
-        errmsg = ['NO_FILE: Attempt to use when file not open, or use of an' ...
-            ' invalid file handle, or no spare file handle'];
+        err_msg = ['NO_FILE: Attempt to use when file not open, or use of an ' ...
+            'invalid file handle, or no spare file handle. Some functions that ' ...
+            'return a time will return -1 to mean nothing found, so this is not ' ...
+            'necessarily an error. Check the function description.'];
     case -2
-        errmsg = ['NO_BLOCK: Failed to allocate a disk block when writing to' ...
+        err_msg = ['NO_BLOCK: Failed to allocate a disk block when writing to ' ...
             'the file. The disk is probably full, or there was a disk error.'];
-    %case -3
 
-    %case -5
+    case -3
+        err_msg = ['CALL_AGAIN: This is a long operation, call again. If you see ' ...
+            'this, something has gone wrong as this is for internal SON library use only.'];
+
+    case -5
+        err_msg = ['NO_ACCESS: This operation was not allowed. Bad access ' ...
+            '(privilege violation), file in use by another process.'];
+
+    case -8
+        err_msg = 'NO_MEMORY: Out of memory reading a 32-bit SON file.';
 
     case -9
-        errmsg = 'NO_CHANNEL: A channel does not exist.';
+        err_msg = 'NO_CHANNEL: A channel does not exist.';
+
     case -10
-        errmsg = 'CHANNEL_USED: Attempt to reuse a channel that already exists.';
+        err_msg = 'CHANNEL_USED: Attempt to reuse a channel that already exists.';
+
+    case -11
+        err_msg = 'CHANNEL_TYPE: The channel cannot be used for this operation.';
+
+    case -12
+        err_msg = ['PAST_EOF: Read past the end of the file. This probably means ' ...
+            'that the file is damaged or that there was an internal library error.'];
+
     case -13
-        errmsg = '-13 WRONG_FILE Attempt to open wrong file type. This is not a SON file.';
+        err_msg = 'WRONG_FILE: Attempt to open wrong file type. This is not a SON file.';
+
+    case -14
+        err_msg = 'NO_EXTRA: A request to read user data is outside the extra data region.';
+
+    case -17
+        err_msg = 'BAD_READ: A read error was detected. This is an operating system error.';
+
+    case -18
+        err_msg = 'BAD_WRITE: Something went wrong writing data. This is an operating system error.';
+
+    case -19
+        err_msg = 'CORRUPT_FILE: The file is bad or an attempt to write corrupted data.';
+
+    case -20
+        err_msg = ['PAST_SOF: An attempt was made to access data before the start ' ...
+            'of the file. This probably means that the file is damaged, or there ' ...
+            'was an internal library error.'];
+
     case -21
-        errmsg = 'READ_ONLY: Failed to open a read-only file for writing';
+        err_msg = 'READ_ONLY: Attempt to write to a read only file.';
+
+    case -22
+        err_msg = 'BAD_PARAM: A bad parameter to a call into the SON library.';
+
+    case -23
+        err_msg = 'OVER_WRITE: An attempt was made to over-write data when not allowed.';
+
+    case -24
+        err_msg = ['MORE_DATA: A file is bigger than the header says; maybe not ' ...
+            'closed correctly. Use S64Fix on it.'];
+
     otherwise
-        error('unrecognized error code')
+        error('Unrecognized CEDS64 error code: %d', iErrorCode)
+end
 end
 
-return
-
-%-------------------------------------
-%       Early exit due to bug
-%-------------------------------------
-
-
-%CEDS64ERRORMESSAGE This function converts integer error codes into warnings
-%describing the errors in plain english.
-%   [ ] = CEDS64ErrorMessage( iErrorCode )
-%   Inputs
-%   iErrorCode - An negative integer code.
-%   Outputs nothing, just generates a warning
-if (isnumeric(iErrorCode) && iErrorCode < 0)
-    %step 1 find out how big the title is is
-    dummystring = blanks(1);
-    [iSize] = calllib('ceds64int', 'S64GetErrorMessage', iErrorCode, dummystring, -1);
-    %step 2 create a string buffer of the correct size
-    errmsg = blanks(iSize+1);
-    calllib('ceds64int', 'S64GetErrorMessage', iErrorCode, errmsg, 0);
-    %???????????????????????
-    %??? Why is this not working?
-
-    %step 3 generate the warning message
-    %warning(errmsg);
-else
-    %
-    error('Unexpected error code: %d',iErrorCode)
-end
-
-
-
-end
 
 %{
 
